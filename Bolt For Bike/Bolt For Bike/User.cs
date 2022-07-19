63d8b89550fdf1b67f446ba5b81500b9cf5a1976
John28306 <j.square28306@gmail.com>
2022-07-19 14:57:13 +0100

    project

---
+++ Bolt For Bike/Bolt For Bike/User.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bolt_For_Bike
+{
+    internal class User
+    {
+        public string Email { get; set; }
+        public string Password { get; set; }
+
+        public User(string email, string password)
+        {
+            Email = email;
+            Password = password;
+        }
+    }
+}
